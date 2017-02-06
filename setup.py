a4afd462f09b387c343de7b93d20ac8ce7c3d5cd
Jökull Sólberg Auðunsson <jokull@solberg.is>
2017-02-06 22:51:30 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from pip.req import parse_requirements
 from distutils.core import setup
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
 
 setup(
     name='Doppler',
     version='0.1dev',
-    packages=[str(ir.req) for ir in install_reqs],
+    url='http://github.com/TakumiHQ/doppler',
+    author='Jokull Solberg Audunsson',
+    packages=['doppler'],
+    install_requires=[
+        'Flask>=0.10',
+        'rpqueue>=0.26.0',
+        'requests>=2.13.0',
+        'redis>=2.10.5',
+    ],
     license='MIT',
     long_description=open('README.md').read(),
 )
